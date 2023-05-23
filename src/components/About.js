25952e100a5f8556f04cfca4706becb31fe3d125
Justin Bigishiro <justin.rvd@gmail.com>
2023-05-23 16:35:35 -0500

    adds myfile

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function BioCondition(props) {
+  if (!props.bio) {
+    return null;
+  } else {
+    return <p>{props.bio}</p>;
+  }
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <BioCondition bio={user.bio} />
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
